d5f0025494b0f66e4f775484c6fd5376291f112b
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2024-02-24 13:22:07 +0200

    Routing

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+export default function Home()
+{
+    return (
+    <>
+        <h2>Home page</h2>
+        <p>This is Home page</p>
+    </>
+    )
+}
---
+++ src/components/Menu.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+export default function Menu()
+{
+return (
+<>
+        <h2>Menu</h2>
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/privacy">Privacy</Link>
+
+        </nav>
+</>
+
+
+)
+
+
+}
